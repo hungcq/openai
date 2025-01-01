da33b462d276d89601416c64e5231728898a6e69
Hung Chu <hung.chuquoc@ninjavan.co>
2025-01-01 12:45:45 +0700

    init

---
+++ index.mjs
@@ -0,0 +1,18 @@
+import "dotenv/config";
+
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+            role: "user",
+            content: "Write a very short poem about coral vine and cat",
+        },
+    ],
+});
+
+console.log(completion);
+console.log(completion.choices[0].message);
